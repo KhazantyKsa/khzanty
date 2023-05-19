3c6fd00e09e114ac3e68b230c38fbc3d45f0fefa
KhazantyKsa <131912217+KhazantyKsa@users.noreply.github.com>
2023-05-19 22:22:23 +0300

    Update Location.jsx

--- components/Location.jsx
+++ components/Location.jsx
@@ -16,9 +16,9 @@ const contactInfo = [
   },
   {
     id: 2,
-    text: "+9660565703612",
+    text: "+9660570776738",
     icon: <BsFillTelephoneFill className='m-auto' color='green' />,
-    href: "tel:+9660565703612",
+    href: "tel:+9660570776738",
   },
   {
     id: 3,
