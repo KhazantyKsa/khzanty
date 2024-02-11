9971172246f65478014b2cb0e7a04d3254c2edb4
KhazantyKsa <131912217+KhazantyKsa@users.noreply.github.com>
2024-02-11 23:57:07 +0200

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -23,6 +23,22 @@ export default function Home() {
           name='keywords'
           content='اثاث, خزانه, خزانات, مقعد, مقاعد, سرير, سراير, طاوله, طاولات, اثاث منزلي , صيانة اثاث, غرف نوم'
         />
+        <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-WJ8VH84DSP"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-WJ8VH84DSP');
+</script>
+          <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "l05c30ta68");
+</script>
       </Head>
       <Slider />
       {/*components*/}
