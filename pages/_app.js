a8a19eb1a41ff8da469371f8c132e84b906cd830
KhazantyKsa <131912217+KhazantyKsa@users.noreply.github.com>
2024-02-12 00:55:48 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,15 +4,6 @@ import NavBar from '../components/NavBar';
 export default function App({ Component, pageProps })
 {
   return (<>
-     <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-WJ8VH84DSP"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-WJ8VH84DSP');
-</script>
     <NavBar />
     <Component { ...pageProps } />
   </>
